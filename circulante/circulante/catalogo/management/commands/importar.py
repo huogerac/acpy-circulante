4cbb7dfde8cb71c82b8775c1c7b4ca4fcc06fdb9
Roger Camargo <huogerac@gmail.com>
2012-04-19 22:36:46 -0300

    importação parcial do arquivo

--- circulante/circulante/catalogo/management/commands/importar.py
+++ circulante/circulante/catalogo/management/commands/importar.py
@@ -4,6 +4,8 @@ import io
 
 from django.core.management.base import BaseCommand, CommandError
 
+from circulante.catalogo.models import Publicacao
+
 class Command(BaseCommand):
     args = '<arq_delimitado_por_tabs> [<encoding>]'
     help = 'Importa massa de dados da livraria (encoding default=utf-8)'
@@ -20,14 +22,32 @@ class Command(BaseCommand):
             encoding = args[1]
         
         with io.open(nome_arq, 'rt', encoding=encoding) as arq_ent:
-            linhas = arq_ent.readlines()
+            qt_registros = 0
+            try:
+                #linhas = arq_ent.readlines()
+                for linha in arq_ent:
+                    linha = linha.rstrip()
+                    if not linha:
+                        continue
+                    partes = linha.split('\t')
+                    
+                    id_padrao = None
+                    autores = ''
+                    
+                    if len(partes) >= 3:
+                        id_padrao, num_paginas, titulo = partes[:3]
+                    if len(partes) == 4:
+                        autores = partes[3]
+                    
+                    if id_padrao is None:
+                        raise CommandError( repr(partes) )
+                    
+                    qt_registros += 1 
+                
+            except UnicodeDecodeError as exc:
+                # para ativar uma linha de breakpoint para debug
+                #import pdb; pdb.set_trace()
+                msg = u'Encondig incorreto: "{0.reason}" posicao:{0.start}'
+                raise CommandError(msg.format(exc))
             
-        self.stdout.write('Importando %s linhas\n' % len(linhas))
-        
-        #for poll_id in args:
-        #    try:
-        #        poll = Poll.objects.get(pk=int(poll_id))
-        #    except Poll.DoesNotExist:
-        #        raise CommandError('Poll "%s" does not exist' % poll_id)
-        #    poll.opened = False
-        #    poll.save()
+        self.stdout.write('Importando %s linhas\n' % qt_registros)
