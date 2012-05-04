da7571c503efaa597e1ebd2260bdd71de8f52d5a
Roger Camargo <huogerac@gmail.com>
2012-05-03 22:41:09 -0300

    novos modelos

--- circulante/circulante/emprestimo/models.py
+++ circulante/circulante/emprestimo/models.py
@@ -4,14 +4,43 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
+from circulante.catalogo.models import Publicacao
+
+
 class Participante(models.Model):
-        user = models.OneToOneField(User)
-        ativo = models.BooleanField(default=True)
+    user = models.OneToOneField(User)
+    ativo = models.BooleanField(default=True)
 
+class Clube(models.Model):
+    nome = models.CharField(max_length=64)
+    descricao = models.CharField(max_length=1024)
+    membros = models.ManyToManyField(Participante)    
 
-# definition of UserProfile from above
-# ...
+OPCOES_STATUS =  [
+    ('disponivel', u'disponível'),  #disponível para empréstimo
+    ('emprestado', u'emprestado'),  #emprestado no momento
+    ('reservado', u'reservado'),    #não disponível para empréstimo
+    ('privado', u'privado'),        #não disponível e não listado
+                  ]
 
+class Item(models.Model):
+    data_aquisicao = models.DateField()
+    publicacaco = models.ForeignKey(Publicacao)
+    proprietario = models.ForeignKey(Participante)
+    status = models.CharField(max_length=16, choices=OPCOES_STATUS, 
+                              default=OPCOES_STATUS[0][0])
+    notas = models.TextField(blank=True)
+    
+class Emprestimo(models.Model):
+    solicitante = models.ForeignKey(Participante)
+    publicacaco = models.ForeignKey(Publicacao)
+    item = models.ForeignKey(Item, null=True)
+    datahora_retirada = models.DateTimeField(null=True)
+    datahora_devolucao = models.DateTimeField(null=True)
+    data_limite_devolucao = models.DateField(null=True)
+    notas = models.TextField(blank=True)
+    
+# SIGNALS
 def criar_registro_participante(sender, instance, created, **kwargs):
     if created:
         Participante.objects.create(user=instance)
